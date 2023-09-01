e9cf04e347cbb7bda57481d7f435742a98365c26
Hicham LAQRAFI <53710462+Hichamrex@users.noreply.github.com>
2023-09-01 10:24:52 +0100

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -32,17 +32,6 @@ def dockerPush(String imageName) {
     sh "docker push $imageName"
 }
 
-// def deployApplication(String imageName) {
-//     echo "Deploying the application to EC2..."
-//     def shellCmd = "bash ./server-script-back.sh $imageName"
-//      withCredentials([usernamePassword(credentialsId: 'azure-vm', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
-//         sh "scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no ./server-script-back.sh $USERNAME@20.166.72.53:/home/$USERNAME"
-//         sh "scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no ./docker-compose-spring.yaml $USERNAME@20.166.72.53:/home/$USERNAME"
-//         sh "echo $PASSWORD | sshpass --password-stdin ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $USERNAME@20.166.72.53 ${shellCmd}"
-// }
-
-// }
-
 def deployApplication(String imageName) {
     echo "Deploying the appication to EC2..."
     def shellCmd = "bash ./server-script-back.sh $imageName"
